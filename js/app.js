8568a026c184e6db0e229b9c8f6a1bc7a51bdaa8
Alexalom99 <alexalom1999@gmail.com>
2025-03-27 21:24:24 +0100

    done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
 // Tu código aquí.
+let contador = document.querySelector("#counter");
+
+let btnlow = document.querySelector(".btn-container > button:first-child");
+let btnadd = document.querySelector(".btn-container > button:last-child");
+
+
+
+// btnlow.addEventListener("click",function () {
+//     contador.textContent--;
+//     if (contador.textContent < 0) {
+//         contador.style.color = "red";
+//     }
+//     else if (contador.textContent == 0) {
+//         contador.style.color = "var(--mainBlack)"
+//     }
+// } );
+
+// btnadd.addEventListener("click",function () {
+//     contador.textContent++;
+//     if (contador.textContent > 0) {
+//         contador.style.color = "green";
+//     }
+//     else if (contador.textContent == 0) {
+//         contador.style.color = "var(--mainBlack)"
+//     }
+// });
+
+
+btnlow.addEventListener("click",function () {
+    contador.textContent--;
+    if (contador.textContent < 0) {
+        contador.classList.add("negative")
+    }
+    else if (contador.textContent == 0) {
+        contador.classList.remove("positive")
+    }
+} );
+
+btnadd.addEventListener("click",function () {
+    contador.textContent++;
+    if (contador.textContent > 0) {
+        contador.classList.add("positive")
+    }
+    else if (contador.textContent == 0) {
+        contador.classList.remove("negative")
+    }
+});
